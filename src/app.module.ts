19f1d7b5e8f09f455ed1ae31047915a245497d83
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-25 08:09:37 +0200

    feat(db): register mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { API_MONGODB_URI } from './common/constants';
 import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), LogModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (service: ConfigService) => ({
+        uri: service.get<string>(API_MONGODB_URI),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    LogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
