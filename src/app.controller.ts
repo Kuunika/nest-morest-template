79f1dbfabacae31ccb882b317b458d0efdaef9b9
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-25 08:09:19 +0200

    refactor(app): bind interceptor to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LogInterceptor } from './log/interceptors/log.interceptor';
 
+@UseInterceptors(LogInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseInterceptors(LogInterceptor)
   @Get()
   getHello(): string {
     return this.appService.getHello();
